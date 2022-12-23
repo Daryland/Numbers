2365188416a393657a2d300ed09979a5eb96a5d2
foesublime <rylandcs@gmail.com>
2022-12-23 12:19:57 -0700

    added info to JS index

---
+++ index.js
@@ -0,0 +1,24 @@
+// JavaScript Numbers only accurate to 15 
+
+var totalUsers = 422345343234532;
+var purchase = 46.99
+
+var sciNot = 123e5;
+var sciNott = 123e-5;
+
+// all common math operations work in JS
+
+var numVal = 5 + 5;
+
+// remainder operation
+var numVal = 17 % 5;
+
+
+
+
+
+
+
+
+
+console.log()
